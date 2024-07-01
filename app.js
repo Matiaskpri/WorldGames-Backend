e29aa209becf3a5421ae93436276d977ff693b20
Matias <matiaskpri07@gmail.com>
2024-07-01 10:18:11 -0300

    commit-1

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const gamesRoutes = require('./src/routers/gamesRoutes');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send('Hola Mundo!');
-    });
+app.use(express.json());
+app.use('/games',gamesRoutes);
 
 
 app.listen(port , ()=>{
-    console.log('el servidor esta encendido en http://localhost:3000');
+    console.log(`el servidor esta encendido en http://localhost:${port}`);
 });
---
+++ src/controllers/gamesController.js
@@ -0,0 +1,54 @@
+const db = require('../db/db');
+
+const getAllGames = (req,res)=> {
+    const sql = 'SELECT * FROM games';
+    db.query(sql,(err,results)=>{
+        if(err)throw err;
+        res.json(results);
+    })
+};
+
+const getGameId = (req,res)=> {
+    const{id} = req.params;
+    const sql = 'select * from games where id =?';
+    db.query(sql,[id],(err,result)=>{
+        if(err)throw err;
+        res.json(result);
+    });
+};
+
+const createGame = (req,res)=> {
+    const {titulo, consola, genero} = req.body;
+    const sql = 'insert into games (id,titulo, consola, genero) values(?,?,?)';
+    db.query(sql,[titulo, consola, genero],(err,result)=>{
+        if(err)throw err;
+        res.json({message:'game creado', gameid: result.insertId});
+    });
+};
+
+const updateGame = (req,res)=> {
+    const {id} = req.params;
+    const {titulo, consola, genero} = req.body;
+    const sql = 'update games set titulo = ?, consola = ?, genero = ? where id = ?';
+    db.query(sql,[id,titulo, consola, genero],(err,results)=>{
+        if(err)throw err;
+        res.json({message: 'game actualizado'});
+    });
+};
+
+const deleteGame = (req,res)=> {
+    const {id} = req.params;
+    const sql = 'delete from games where id= ?';
+    db.query(sql,[id],(err,results)=>{
+        if(err)throw err;
+        res.json({message:'game borrado'});
+    });
+};
+
+module.exports= {
+    getAllGames,
+    getGameId,
+    createGame,
+    updateGame,
+    deleteGame
+};
